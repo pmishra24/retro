559e3fef0242a1a7abfe30762a410116edb2d530
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2020-01-10 18:58:28 +0530

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,5 +1,9 @@
 import { Component , ElementRef, ViewChild} from '@angular/core';
 import {ExcelService} from './services/excel.service';
+import { HttpClient } from '@angular/common/http';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+const { read, write, utils } = XLSX;
 
 @Component({
   selector: 'app-root',
@@ -8,7 +12,12 @@ import {ExcelService} from './services/excel.service';
 })
 export class AppComponent {
   title = 'Retro'; 
+i:number = 0;
+j:number = 0;
+k:number = 0;
+p:number = 1;
 
+data: any = [];
 Wentwell : any = [];
 Toimprove : any = [];
 Actionitem : any = [];
@@ -35,34 +44,98 @@ idaction:number=-1;
 HeadingEditableimprove:boolean=false;
 HeadingEditabletitle:boolean=false;
 HeadingEditableaction:boolean=false;
-constructor(private excelService:ExcelService){
 
+keepgoing:boolean= true;
+
+constructor(private excelService:ExcelService, private http :HttpClient){
 }
 
-data: any = [
-   {
-    HeadingWent: this.HeadingWent,
-    HeadingImprove: this.HeadingImprove,
-    HeadingAction: this.HeadingAction
-  },
-  {
-    HeadingWent: "ho",
-    HeadingImprove: 'ravi',
-    HeadingAction: 1000
-  },
-  {
-    HeadingWent: this.HeadingImprove,
-    HeadingImprove: 'ram',
-    HeadingAction: 2000
-  },
-  {
-    HeadingWent: 'e103',
-    HeadingImprove: 'rajesh',
-    HeadingAction: 3000
-  }];
+// ngOnInit(): void {
+//     console.log("I'll be called when you first load the page, but NOT if you come to this page by pressing the 'back' button on another page");
+//     // this.onFileChange("C:\\Users\\761418\\Downloads\\sample_export_1577798777498.xlsx");
+//     this.http.get('assets/sample_export_1577798777498.xlsx', {responseType: 'text'}).subscribe(data => {
+//     console.log(data);
+// })
+//   }
+// data1: any = [[1, 2], [3, 4]];
+// onFileChange(evt: any) {
+//     /* wire up file reader */
+//     console.log(<DataTransfer>(evt.target));
+//     const target: DataTransfer = <DataTransfer>(evt.target);
+//     if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+//     const reader: FileReader = new FileReader();
+//     reader.onload = (e: any) => {
+//       /* read workbook */
+//       const bstr: string = e.target.result;
+//       const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+//       /* grab first sheet */
+//       const wsname: string = wb.SheetNames[0];
+//       const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+//       /* save data */
+//       this.data1 = <any>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+//       console.log(this.data1);
+//     };
+//     reader.readAsBinaryString(target.files[0]);
+//   }
 
 ExportTOExcel():void {  
-    this.excelService.exportAsExcelFile(this.data, 'Serviscope Retro');
+          this.keepgoing= true;
+          this.data = []
+          this.data[0]= {
+              HeadingWent: this.HeadingWent,
+              HeadingImprove: this.HeadingImprove,
+              HeadingAction: this.HeadingAction};
+
+          while(this.keepgoing)
+          {
+            if(this.i<this.Wentwell.length && this.k<this.Actionitem.length && this.j<this.Toimprove.length)
+            {
+              this.data[this.p] = {HeadingWent:this.Wentwell[this.i],HeadingImprove:this.Toimprove[this.j],HeadingAction:this.Actionitem[this.k]};
+              this.i++;this.j++;this.k++;this.p++;
+            }
+            else
+            {
+              if(this.i<this.Wentwell.length && this.k<this.Actionitem.length)
+              {
+                this.data[this.p] = {HeadingWent:this.Wentwell[this.i],HeadingImprove:"",HeadingAction:this.Actionitem[this.k]};
+                this.i++;this.k++;this.p++;
+              }
+              else if(this.k<this.Actionitem.length && this.j<this.Toimprove.length)
+              {
+                this.data[this.p] = {HeadingWent:"",HeadingImprove:this.Toimprove[this.j],HeadingAction:this.Actionitem[this.k]};
+                this.j++;this.k++;this.p++;
+              }
+              else if(this.i<this.Wentwell.length && this.j<this.Toimprove.length)
+              {
+                this.data[this.p] = {HeadingWent:this.Wentwell[this.i],HeadingImprove:this.Toimprove[this.j],HeadingAction:""};
+                this.i++;this.j++;this.p++;
+              }
+              else if(this.i<this.Wentwell.length)
+              {
+                this.data[this.p] = {HeadingWent:this.Wentwell[this.i],HeadingImprove:"",HeadingAction:""};
+                this.i++;this.p++;
+              }
+              else if(this.j<this.Toimprove.length)
+              {
+                this.data[this.p] = {HeadingWent:"",HeadingImprove:this.Toimprove[this.j],HeadingAction:""};
+              this.j++;this.p++;
+              }
+              else if(this.k<this.Actionitem.length)
+              {
+                this.data[this.p] = {HeadingWent:"",HeadingImprove:"",HeadingAction:this.Actionitem[this.k]};
+                this.k++;this.p++;
+              }
+              else
+              {
+                this.keepgoing = false;
+              }
+            }
+          }
+        this.i = 0;this.j = 0;this.k = 0;this.p=1;
+        console.log(this.data)
+        this.excelService.exportAsExcelFile(this.data, 'Serviscope Retro');
   }
 
 editHeading(type:any){
@@ -130,17 +203,17 @@ editHeading(type:any){
 
  
   showTextwent(toadd: any){
-    this.Wentwell.push({name: toadd});
+    this.Wentwell.push(toadd);
     this.isaddwent=false;
   }
 
   showTextimprove(toadd: any){
-    this.Toimprove.push({name: toadd});
+    this.Toimprove.push(toadd);
     this.isaddimprove=false;
   }
 
   showTextaction(toadd: any){
-    this.Actionitem.push({name: toadd});
+    this.Actionitem.push(toadd);
     this.isaddaction=false;
   }
 
@@ -191,21 +264,21 @@ editaction(id:number)
 
 editTextwent(id: any, edited : any)
 {
-  this.Wentwell[id] = {name: edited};
+  this.Wentwell[id] = edited;
   console.log(this.Wentwell[id] );
     this.iseditenableWent = false;
     this.idwent=-1;    
 }
 editTextimprove(id: any, edited : any)
 {
-  this.Toimprove[id] = {name: edited};
+  this.Toimprove[id] = edited;
   console.log(this.Toimprove[id] );
     this.iseditenableimprove = false;
     this.idimprove=-1;
 }
 editTextaction(id: any, edited : any)
 {
-  this.Actionitem[id] = {name: edited};
+  this.Actionitem[id] = edited;
   console.log(this.Actionitem[id] );
     this.iseditenableaction = false;
     this.idaction=-1;
